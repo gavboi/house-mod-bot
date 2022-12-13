7f7b08a854da6297845fec220e395a9576f2c185
gavboi <gj1087@hotmail.com>
2022-12-13 15:30:46 -0500

    ported dice and anagram functions

--- v2/bot.py
+++ v2/bot.py
@@ -1,79 +1,122 @@
-# imports
+# IMPORTS
 from datetime import datetime
 from sys import platform
 from os import popen
+import random
+from importlib import reload
+import funcs
 
-# logging
+# LOGGING
 def get_now():
-	now = datetime.now()
-	return now.strftime('%Y-%b-%d %H:%M:%S')
+    now = datetime.now()
+    return now.strftime('%Y-%b-%d %H:%M:%S')
 
 logfile = open('bot.log', 'w')
-	
+    
 def log(text=''):
-	text = '[' + get_now() + '] ' + str(text)
-	print(text)
-	logfile.write(text + '\n')
+    text = '[' + get_now() + '] ' + str(text)
+    print(text)
+    logfile.write(text + '\n')
 
 log('Starting...')
 
-# non-logging helpers
+# NON-LOGGING HELPERS
 def stop():
-	log('Exiting...')
-	logfile.close()
-	exit()
+    log('Exiting...')
+    logfile.close()
+    exit()
 
-# import installed libraries
+# IMPORT INSTALLED LIBRARIES
 try:
-	import discord
-	from discord import app_commands
-	log('discord.py found.')
+    import discord
+    from discord import app_commands
+    log('discord.py found.')
 except:
-	log('discord.py not found. Attempting to acquire...')
-	if platform == 'win32':
-		stream = popen('py -3 -m pip install -U discord.py')
-	else:
-		stream = popen('python3 -m pip install -U discord.py')
-	log(stream.read())
-	try:
-		import discord
-		from discord import app_commands
-		log('discord.py found.')
-	except:
-		log('discord.py could not be found or acquired.')
-		stop()
-		
-# setup
+    log('discord.py not found. Attempting to acquire...')
+    if platform == 'win32':
+        stream = popen('py -3 -m pip install -U discord.py')
+    else:
+        stream = popen('python3 -m pip install -U discord.py')
+    log(stream.read())
+    try:
+        import discord
+        from discord import app_commands
+        log('discord.py found.')
+    except:
+        log('discord.py could not be found or acquired.')
+        stop()
+        
+# SETUP
 class MyClient(discord.Client):
 
-	def __init__(self):
-		intents = discord.Intents.default()
-		intents.message_content = True
-		super().__init__(intents=intents)
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
 
-	async def on_ready(self):
-		await self.wait_until_ready()
-		log(f'Logged on as {self.user}.')
+    async def on_ready(self):
+        await self.wait_until_ready()
+        log(f'Logged on as {self.user}.')
 
-	async def on_message(self, message):
-		log(f'({message.guild}) {message.author}: {message.content}')
-		if message.content.startswith('$sync'):
-			if message.author.id == 405902825265299456:
-				await message.channel.send('Syncing commands...')
-				await tree.sync()
-				log('Synced commands.')
-			else:
-				await message.channel.send('Only the owner can use this command!')
-		#if message.author == client.user: return
+    async def on_message(self, message):
+        log(f'({message.guild}) {message.author}: {message.content}')
+        
+        if message.content.startswith('$sync'):
+            if message.author.id == 405902825265299456:
+                # if message.content.endswith('here'):
+                    # await message.channel.send(f'Syncing commands in {message.guild}...')
+                    # await tree.sync(guild=message.guild)
+                # else:
+                await message.channel.send('Syncing commands globally...')
+                await tree.sync()
+                log('Synced commands.')
+            else:
+                await message.channel.send('Only the owner can use this command!')
+                
+        if message.content.startswith('$reload'):
+            if message.author.id == 405902825265299456:
+                await message.channel.send('Reimporting funcs...')
+                reload(funcs)
+            else:
+                await message.channel.send('Only the owner can use this command!')
+                
+        if message.content.startswith('$stop'):
+            if message.author.id == 405902825265299456:
+                await message.channel.send('byeeeee')
+                stop()
+            else:
+                await message.channel.send('Only the owner can use this command!')
+        # if message.author == client.user: return
 
 client = MyClient()
 tree = app_commands.CommandTree(client)
 
-# commands
-@tree.command(name = 'test', description = 'testing')
+# COMMANDS
+@tree.command(name='test', description='Testing')
 async def self(interaction: discord.Interaction, arg: str):
-	# log caller
-	await interaction.response.send_message(f'Did you give me this "{arg}"?')
+    log(f'({interaction.guild}) {interaction.user}: {interaction.data}')
+    await interaction.response.send_message(f'Did you give me this "{arg}"?')
+    
+@tree.command(name='roll', description='Roll a d-sided die n times')
+async def self(interaction: discord.Interaction, d: int=6, n: int=1):
+    log(f'({interaction.guild}) {interaction.user}: {interaction.data}')
+    rolls = []
+    msg = f'({n}d{d}) Your rolls: '
+    for _ in range(n):
+        rolls.append(random.randint(1, d))
+        msg += '`' + str(rolls[-1]) + '` '
+    if n > 1:
+        msg += '\nTotal: `' + str(sum(rolls)) + '`'
+    await interaction.response.send_message(msg)
 
-# run
+@tree.command(name='unscramble',description='Rearrange letters to form words')
+async def self(interaction: discord.Interaction, letters: str):
+    log(f'({interaction.guild}) {interaction.user}: {interaction.data}')
+    ans, s = funcs.search_word(letters)
+    if len(s) == 0:
+        s = 'n/a'
+    await interaction.response.send_message(f'({letters}) Results: {s}')
+    
+
+# RUN
 client.run('Nzg5Nzk3MjU2OTIwMzAxNTg5.X93SAw.1JeH6wuj92pyESjbYVYlYFHKC-c')
